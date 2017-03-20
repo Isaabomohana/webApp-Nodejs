429cb718548e0ce8c475cfc92e0a858627af64de
Isaabomohana <mrissaam@outlook.com>
2017-03-20 11:12:00 +0100

    add express.static to routing

--- server.js
+++ server.js
@@ -1,38 +1,11 @@
 var express = require("express");
 var app = express();
-var router = express.Router();
-var path = __dirname + '/views/';
+var path = require('path');
 require('dotenv').config();
 
 var config = require('./config');
 
-
-router.use(function (req,res,next) {
-  console.log("/" + req.method);
-  next();
-});
-
-router.get("/",function(req,res){
-  res.sendFile(path + "index.html");
-});
-
-router.get("/about",function(req,res){
-  res.sendFile(path + "about.html");
-});
-
-app.use("/ourteam",function(req,res){
-  res.sendFile(path + "ourteam.html");
-});
-
-router.get("/contact",function(req,res){
-  res.sendFile(path + "contact.html");
-});
-
-app.use("/",router);
-
-app.use("*",function(req,res){
-  res.sendFile(path + "404.html");
-});
+app.use(express.static(path.join(__dirname, '/public')))
 
 app.listen(process.env.PORT || 3000,function(){
   console.log("Live at Port 3000");
